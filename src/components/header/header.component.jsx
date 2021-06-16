7a88ec8560bc2e523cba495ed36253e0fdc539bd
MJ <mohamed.aref32@gmail.com>
2021-06-16 13:16:42 -0500

    verifying user auth from the db and updating the state from user profile document

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
+import {connect} from "react-redux";
 import {auth} from "../../firebase/firebase.utils";
 import "./header.styles.scss";
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const firebaseConfig = {
   measurementId: "G-1NZF177349",
 };
 
+// getting userAuth data and add it to the db
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(firebaseConfig);
 
 //   exporting firebase auth and firestore to be used in other modules.
