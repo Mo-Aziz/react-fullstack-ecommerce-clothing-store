0252cfa00bd507ffd5728c354f1dfc697209af72
MJ <mohamed.aref32@gmail.com>
2021-06-21 23:26:47 -0500

    implementing selector from reselect and using sliced state on cart icon and cart dropdwon

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 import cartActionTypes from "./cart.types";
 import { addItemToCart } from "./cart.utils";
-const INITIAL_STATE = { hidden: true, cartItems: []};
+const INITIAL_STATE = { hidden: true, cartItems: [] };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -9,11 +9,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden,
       };
-      case cartActionTypes.ADD_ITEM:
-        return{
-          ...state,
-          cartItems:addItemToCart(state.cartItems, action.payload)
-        }
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
